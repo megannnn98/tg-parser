d0e9a50c2e5df482e7999cf6322f1addfd67ccf7
m.karatanov <karatanov.mn@gmail.com>
2026-01-17 14:46:21 +0500

    feat: reader works well

--- main.py
+++ main.py
@@ -7,14 +7,28 @@ from parser.storage import save_message, save_user
 from config import CHANNEL_USERNAME
 
 async def main():
-    db = await get_db()
-    await init_db(db)
 
     app = get_client()
-    await app.start()
-    print("Logged in")
 
-    await app.stop()
+    async with app:
+        channel = await app.get_chat(CHANNEL_USERNAME)
+        print("Logged in, channel:", channel.title)
+
+        if not channel.linked_chat:
+            print("Failed to get linked chat")
+            return
+
+        discussion_id = channel.linked_chat.id
+
+        db = await get_db()
+        await init_db(db)
+
+        async for user in fetch_users(app, discussion_id):
+            await save_user(db, user)
+
+        await db.commit()
+        await db.close()
+        print("Done")
 
 if __name__ == "__main__":
     asyncio.run(main())
