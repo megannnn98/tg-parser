8cade9b4b1732343770b79a47aef638e048d3e2b
m.karatanov <karatanov.mn@gmail.com>
2026-01-27 08:56:10 +0500

    feat: added search of haters

--- main.py
+++ main.py
@@ -5,6 +5,59 @@ from parser.collector import collect_channel
 from parser.utils import parse_channels, db_path_for_channel
 from config import CHANNELS
 from parser.logger import get_logger
+import aiosqlite
+import re
+
+async def get_user_messages_from_db(
+    db_path: str,
+    tg_id: int
+) -> list[str]:
+    """
+    Возвращает все сообщения пользователя (по tg_id)
+    из конкретной базы данных.
+    """
+
+    async with aiosqlite.connect(db_path) as db:
+        async with db.execute(
+            """
+            SELECT m.text
+            FROM messages m
+            JOIN users u ON u.id = m.user_id
+            WHERE u.tg_id = ?
+            ORDER BY m.id
+            """,
+            (tg_id,)
+        ) as cursor:
+            return [row[0] async for row in cursor]
+    return []
+
+async def get_users_from_db(db_path: str) -> list[int]:
+    async with aiosqlite.connect(db_path) as db:
+        async with db.execute(
+            "SELECT tg_id FROM users"
+        ) as cursor:
+            return [row[0] async for row in cursor]
+    return []
+
+async def get_haters_from_db(db_path: str) -> list[int]:
+    haters = []
+    users = await get_users_from_db(db_path="data/ru_doy.db")
+
+    for user in users:
+        messages = await get_user_messages_from_db(
+            db_path="data/ru_doy.db",
+            tg_id=user
+        )
+
+        for msg in messages:
+            word = "рудуа"
+            found = bool(re.search(r'\b{}\b'.format(re.escape(word)), msg, re.IGNORECASE))
+            if found:
+                haters.append(user)
+                break
+
+    return haters
+
 
 async def main():
     channels = parse_channels(CHANNELS)
@@ -14,19 +67,46 @@ async def main():
     tg_client = get_client()
     logger = get_logger("main")
 
-    async with tg_client:
-        for channel in channels:
-            db_path = db_path_for_channel(channel)
-            db = await get_db(db_path)
+    haters = await get_haters_from_db(db_path="data/ru_doy.db")
+    print(haters)
 
-            try:
-                logger.info(f"Collecting channel {channel}")
-                await init_db(db)
-                await collect_channel(tg_client, db, channel)
-                logger.info(f"Finished collecting channel {channel}")
-            finally:
-                await db.close()
-        logger.info("Done")
+
+
+
+
+
+
+    # async with tg_client:
+    #     for channel in channels:
+    #         db_path = db_path_for_channel(channel)
+    #         db = await get_db(db_path)
+
+    #         try:
+    #             logger.info(f"Collecting channel {channel}")
+    #             await init_db(db)
+    #             await collect_channel(tg_client, db, channel)
+    #             logger.info(f"Finished collecting channel {channel}")
+    #         finally:
+    #             await db.close()
+    #     logger.info("Done")
+
+    # USER_ID = 428434968
+
+    # messages = await get_user_messages_from_db(
+    #     db_path="data/ru_doy.db",
+    #     tg_id=USER_ID
+    # )
+
+    # print("=" * 40)
+    # print(f"Сообщения пользователя {USER_ID}")
+    # print("=" * 40)
+
+    # for msg in messages:
+    #     print("-", msg)
+
+
+
+
 
 if __name__ == "__main__":
     asyncio.run(main())
--- parser/telegram.py
+++ parser/telegram.py
@@ -8,8 +8,8 @@ def get_client():
         api_hash=API_HASH
     )
 
-async def fetch_messages(app, discussion_id):
-    async for msg in app.get_chat_history(discussion_id, LIMIT):
+async def fetch_messages(tg_client, chat_id):
+    async for msg in tg_client.get_chat_history(chat_id, LIMIT):
         if not msg.text or not msg.from_user:
             continue
 
