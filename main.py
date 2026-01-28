f4bf6ef256c43fd3c4acf527a060013a97f11cd9
m.karatanov <karatanov.mn@gmail.com>
2026-01-28 08:56:54 +0500

    feat: split cmds

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import asyncio
-from parser.telegram import get_client
-from parser.database import get_db, init_db
-from parser.collector import collect_channel
-from parser.utils import parse_channels, db_path_for_channel
-from config import CHANNELS
-from parser.logger import get_logger
+from parser.collector import collect_db
 import aiosqlite
 import re
 
@@ -39,74 +34,59 @@ async def get_users_from_db(db_path: str) -> list[int]:
             return [row[0] async for row in cursor]
     return []
 
-async def get_haters_from_db(db_path: str) -> list[int]:
+async def get_haters_from_db(db_path: str, hate_words: list[str]) -> list[int]:
     haters = []
-    users = await get_users_from_db(db_path="data/ru_doy.db")
+    users = await get_users_from_db(db_path)
 
     for user in users:
         messages = await get_user_messages_from_db(
-            db_path="data/ru_doy.db",
+            db_path,
             tg_id=user
         )
 
         for msg in messages:
-            word = "рудуа"
-            found = bool(re.search(r'\b{}\b'.format(re.escape(word)), msg, re.IGNORECASE))
+            found = bool(re.search(r'\b{}\b'.format(re.escape(hate_words[0])), msg, re.IGNORECASE))
             if found:
                 haters.append(user)
                 break
 
     return haters
 
+async def get_haters(db_path: str):
+    hate_words = ["диалектика"]
+    haters = await get_haters_from_db(db_path, hate_words=hate_words)
+    return haters
+
+async def print_user_messages(db_path: str, tg_id: int):
+    messages = await get_user_messages_from_db(
+        db_path,
+        tg_id=tg_id
+    )
+
+    print("=" * 40)
+    print(f"Сообщения пользователя {tg_id}")
+    print("=" * 40)
+
+    for msg in messages:
+        print("-", msg)
+
+
+COLLECT_MESSAGES_FROM_TG = 0
+FIND_HATERS_IN_DB = 1
+
+cmd = FIND_HATERS_IN_DB
 
 async def main():
-    channels = parse_channels(CHANNELS)
-    if not channels:
-        raise RuntimeError("CHANNELS is empty")
 
-    tg_client = get_client()
-    logger = get_logger("main")
+    # db_path = "data/ru_doy.db"
+    db_path = "data/vihod_est.db"
 
-    haters = await get_haters_from_db(db_path="data/ru_doy.db")
-    print(haters)
-
-
-
-
-
-
-
-    # async with tg_client:
-    #     for channel in channels:
-    #         db_path = db_path_for_channel(channel)
-    #         db = await get_db(db_path)
-
-    #         try:
-    #             logger.info(f"Collecting channel {channel}")
-    #             await init_db(db)
-    #             await collect_channel(tg_client, db, channel)
-    #             logger.info(f"Finished collecting channel {channel}")
-    #         finally:
-    #             await db.close()
-    #     logger.info("Done")
-
-    # USER_ID = 428434968
-
-    # messages = await get_user_messages_from_db(
-    #     db_path="data/ru_doy.db",
-    #     tg_id=USER_ID
-    # )
-
-    # print("=" * 40)
-    # print(f"Сообщения пользователя {USER_ID}")
-    # print("=" * 40)
-
-    # for msg in messages:
-    #     print("-", msg)
-
-
-
-
+    if cmd == COLLECT_MESSAGES_FROM_TG:
+        await collect_db()
+    elif cmd == FIND_HATERS_IN_DB:
+        haters = await get_haters(db_path)
+        for hater in haters:
+            await print_user_messages(db_path, tg_id=hater)
 
 if __name__ == "__main__":
     asyncio.run(main())
--- parser/collector.py
+++ parser/collector.py
@@ -1,6 +1,11 @@
 from parser.telegram import fetch_messages
 from parser.storage import upsert_user, save_message
 from parser.logger import get_logger
+from parser.utils import parse_channels, db_path_for_channel
+from parser.telegram import get_client
+from parser.database import get_db, init_db
+from config import CHANNELS
+import aiosqlite
 
 async def collect_channel(tg_client, db, channel_username: str):
     channel = await tg_client.get_chat(channel_username)
@@ -29,3 +34,50 @@ async def collect_channel(tg_client, db, channel_username: str):
         )
 
     await db.commit()
+
+async def get_db_stats(db_path: str) -> tuple[int, int]:
+    async with aiosqlite.connect(db_path) as db:
+        async with db.execute("SELECT COUNT(*) FROM users") as cur:
+            users_count = (await cur.fetchone())[0]
+
+        async with db.execute("SELECT COUNT(*) FROM messages") as cur:
+            messages_count = (await cur.fetchone())[0]
+
+    return users_count, messages_count
+
+
+async def collect_db():
+    channels = parse_channels(CHANNELS)
+    if not channels:
+        raise RuntimeError("CHANNELS are empty")
+
+    tg_client = get_client()
+    logger = get_logger("main")
+
+    async with tg_client:
+        for channel in channels:
+            db_path = db_path_for_channel(channel)
+
+            users_before, messages_before = await get_db_stats(db_path)
+
+            logger.info(
+                f"[{channel}] BEFORE → users={users_before}, messages={messages_before}"
+            )
+
+            db = await get_db(db_path)
+            try:
+                logger.info(f"Collecting channel {channel}")
+                await init_db(db)
+                await collect_channel(tg_client, db, channel)
+                logger.info(f"Finished collecting channel {channel}")
+            finally:
+                await db.close()
+
+            users_after, messages_after = await get_db_stats(db_path)
+
+            logger.info(
+                f"[{channel}] AFTER  → users={users_after} (+{users_after - users_before}), "
+                f"messages={messages_after} (+{messages_after - messages_before})"
+            )
+
+        logger.info("Done")
