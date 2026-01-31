8414f2ec256ff44f54f6d566d457501e5d80f80d
m.karatanov <karatanov.mn@gmail.com>
2026-01-31 11:27:53 +0500

    feat: removed usless database file

--- parser/storage.py
+++ parser/storage.py
@@ -1,3 +1,44 @@
+import aiosqlite
+from pathlib import Path
+
+
+async def get_db(db_path: Path):
+    db_path.parent.mkdir(parents=True, exist_ok=True)
+    return await aiosqlite.connect(str(db_path))
+
+async def init_db(db):
+    await db.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            tg_id INTEGER UNIQUE NOT NULL,
+            username TEXT
+        )
+    """)
+
+    await db.execute("""
+        CREATE TABLE IF NOT EXISTS topic (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            discussion_id INTEGER UNIQUE NOT NULL,
+            title TEXT
+        )
+    """)
+
+    await db.execute("""
+        CREATE TABLE IF NOT EXISTS messages (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            discussion_id INTEGER,
+            message_id INTEGER,
+            user_id INTEGER,
+            date TEXT,
+            text TEXT,
+
+            UNIQUE (discussion_id, message_id),
+            FOREIGN KEY(user_id) REFERENCES users(id),
+            FOREIGN KEY(discussion_id) REFERENCES topic(discussion_id)
+        )
+    """)
+
+    await db.commit()
 
 async def save_message(db, discussion_id, msg, user_id):
     await db.execute(
